6d2496c967a701c2cdf3fb073d627966e04acad4
Torbjørn Auglænd Vilhelmsen <torbjvi@stud.ntnu.no>
2013-09-27 21:32:26 +0200

    Drag and drop improvements

--- sidebar.js
+++ sidebar.js
@@ -151,7 +151,17 @@ Sidebar.LayerList.Button = L.Class.extend({
 		return this._element;
 	}
 });
-
+Sidebar.FileDropper = L.Class.extend({
+	initialize: function (map) {
+		this._container = L.DomUtil.create('div', 'droppable file-dropper');
+		this._container.id = "file-dropper";
+		var text = "Drop GeoJSON or zipped shp, prj and dbf files here to add them to the map."
+		this._container.appendChild(document.createTextNode(text));
+	},
+	addTo: function (el) {
+		el.getElement().appendChild(this._container);
+	}
+});
 Sidebar.Tool = L.Class.extend({
 	_map: null,
 	_element: null,
